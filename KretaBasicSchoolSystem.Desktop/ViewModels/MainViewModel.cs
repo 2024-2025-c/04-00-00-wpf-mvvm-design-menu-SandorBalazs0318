8484ff30b9fcb8817427d75c6807285104ccbd3a
c9sanbal <c9sanbal@SZT3-7-W10>
2024-11-08 09:29:37 +0100

    SchoolClassesViewModel

--- KretaBasicSchoolSystem.Desktop/ViewModels/MainViewModel.cs
+++ KretaBasicSchoolSystem.Desktop/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using FontAwesome.Sharp;
 using KretaBasicSchoolSystem.Desktop.ViewModels.Base;
 using KretaBasicSchoolSystem.Desktop.ViewModels.ControlPanel;
 using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolCitizens;
+using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolClasses;
 
 namespace KretaBasicSchoolSystem.Desktop.ViewModels
 {
@@ -11,20 +12,24 @@ namespace KretaBasicSchoolSystem.Desktop.ViewModels
     {
         private ControlPanelViewModel _controlPanelViewModel;
         private SchoolCitizensViewModel _schoolCitizensViewModel;
+        private SchoolClassesViewModel _schoolClassesViewModel;
 
         public MainViewModel()
         {
             _controlPanelViewModel = new ControlPanelViewModel();
             _schoolCitizensViewModel = new SchoolCitizensViewModel();
+            _schoolClassesViewModel = new SchoolClassesViewModel();
         }
 
         public MainViewModel(
-            ControlPanelViewModel controlPanelViewModel,
-            SchoolCitizensViewModel schoolCitizensViewModel 
+            ControlPanelViewModel controlPanelViewModel, //gol fütő
+            SchoolCitizensViewModel schoolCitizensViewModel,
+            SchoolClassesViewModel schoolClassesViewModel
             )
         {
             _controlPanelViewModel = controlPanelViewModel;
             _schoolCitizensViewModel = schoolCitizensViewModel;
+            _schoolClassesViewModel = schoolClassesViewModel;
 
 
             CurrentChildView = _controlPanelViewModel;
